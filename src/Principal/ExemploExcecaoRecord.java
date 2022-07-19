58d4d5b16246050ca5b67a2e2e5ac84e80d06cfa
Anderson S <anderson0563@gmail.com>
2022-07-19 20:26:55 -0300

    Exemplo do uso do Record

---
+++ src/Principal/ExemploExcecaoRecord.java
@@ -0,0 +1,21 @@
+package Principal;
+
+record Rectangle(double length, double width) { public Rectangle(double length, double width) {
+    if (length <= 0 || width <= 0) {
+        throw new java.lang.IllegalArgumentException(
+                String.format("Invalid dimensions: %f, %f", length,
+                        width)); }
+    this.length = length;
+    this.width = width; }
+}
+
+public class ExemploExcecaoRecord {
+    public static void main(String[] args) {
+        System.out.println("Não gera exceção");
+        Rectangle r1 = new Rectangle(10,10);
+        System.out.println("Primeira exceção");
+        //Rectangle r2 = new Rectangle(0,10);
+        System.out.println("Segunda exceção");
+        Rectangle r3 = new Rectangle(10, 0);
+    }
+}
---
+++ src/Principal/ExemploRecordStatic.java
@@ -0,0 +1,21 @@
+package Principal;
+
+record RectangleStatic(double length, double width) {
+
+    static double goldenRatio;
+
+    static {
+        goldenRatio = (1 + Math.sqrt(5)) / 2;
+    }
+
+    public static RectangleStatic createGoldenRectangle(double width) {
+        return new RectangleStatic(width, width * goldenRatio);
+    }
+}
+
+public class ExemploRecordStatic {
+    public static void main(String[] args) {
+        RectangleStatic r = new RectangleStatic(10, 10);
+
+    }
+}
